881a6a13886953793c25c3195cbde905b1c74a9f
JustATRIK <rarseniip@yandex.ru>
2025-06-16 17:30:55 +0500

    selection screen

---
+++ hollowengine/scripts/selection_menu.se.kts
@@ -0,0 +1,56 @@
+val players by server.players
+val player = players().first()
+
+gui {
+	image("is:textures/gui/selection_screen/bgr_1.png") {
+		size(350.px, 256.px)
+	}
+	entity(player) {
+		scale = 2f
+		offset((-123).px, (-20).px)
+	}
+	image("is:textures/gui/selection_screen/bgr_2.png") {
+		size(350.px, 256.px)
+	}
+	label("Вопрос?") {
+		anchor = Anchor.START
+		offset(5.px, (-70).px)
+		scale = 1.5f
+	}
+	button("", "is:textures/gui/selection_screen/empty.png") {
+		size(230.px, 15.px)
+		offset(15.px, (-20).px)
+		onClick = {
+			close()
+		}
+	}
+	label("Вариант 1") {
+		anchor = Anchor.START
+		offset(7.px, (-25).px)
+		scale = 1.2f
+	}
+	button("", "is:textures/gui/selection_screen/empty.png") {
+		size(230.px, 15.px)
+		offset(15.px, (-0).px)
+		onClick = {
+			close()
+		}
+	}
+	label("Вариант 2") {
+		anchor = Anchor.START
+		offset(7.px, (-5).px)
+		scale = 1.2f
+	}
+	button("", "is:textures/gui/selection_screen/empty.png") {
+		size(230.px, 15.px)
+		offset(15.px, (20).px)
+		onClick = {
+			close()
+		}
+	}
+	label("Вариант 3") {
+		anchor = Anchor.START
+		offset(7.px, (15).px)
+		scale = 1.2f
+	}
+}
